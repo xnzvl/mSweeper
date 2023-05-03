d16d8735346f4187c4cf0ea69f453dc68ad1da74
xnzvl <jakubnezval.jn@gmail.com>
2023-05-03 15:23:39 +0200

    version update

--- mSweeper_package/__init__.py
+++ mSweeper_package/__init__.py
@@ -15,7 +15,7 @@ class Difficulty(Enum):
 DATA_FOLDER: str = os.path.join(os.getcwd(), ".mSweeper")
 
 SOFTWARE_TITLE = ":: mSweeper _"
-SOFTWARE_VERSION = "2.00"
+SOFTWARE_VERSION = "2.10"
 
 WINDOW_PREFIXES = {
     here.Minesweeper_state.UNINITIALIZED: "",

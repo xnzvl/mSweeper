83a39fec851e1b122fd62e387024014a50fa4ca6
xnzvl <jakubnezval.jn@gmail.com>
2023-05-03 16:10:30 +0200

    initial

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import sys
+
 import mSweeper_package as mSweeper
+import project_wrapper.superuser as su
 
 
 def main() -> None:
+    if len(sys.argv) != 1 and len(sys.argv) != 3:
+        return
+
+    if len(sys.argv) == 3:
+        su.print_args()
+
     mSweeper.new_session()
 
 
---
+++ project_wrapper/superuser.py
@@ -0,0 +1,5 @@
+import sys
+
+
+def print_args() -> None:
+    print(sys.argv)
